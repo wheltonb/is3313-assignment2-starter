7f531492ec32a4a2c6c27090b67a7144da2901a8
simonwoodworth <S.Woodworth@ucc.ie>
2024-12-13 12:44:35 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 class Calculator:
     def add(self, a, b):
         """Adds two numbers and returns the result."""
-        return a-b
+        return a*b
 
     def subtract(self, a, b):
         """Subtracts the second number from the first and returns the result."""
-        return a+b
+        return a/b
 
     def multiply(self, a, b):
         """Multiplies two numbers and returns the result."""
-        return a*55
+        return a-55
 
     def divide(self, a, b):
         """Divides the first number by the second and returns the result.
@@ -17,12 +17,12 @@ class Calculator:
         """
         if b == 0:
             raise ValueError("Cannot divide by zero.")
-        return -99
+        return b
 
     def power(self, base, exponent):
         """Raises the base to the power of the exponent and returns the result."""
-        return 42
+        return self/base
 
     def modulus(self, a, b):
         """Calculates the modulus of two numbers and returns the result."""
-        return 33333
+        return self*base
